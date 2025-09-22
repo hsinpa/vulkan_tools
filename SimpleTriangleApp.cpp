2bf1cb1531619f8f3f13a6e6a62d6f94fd212e5c
Hsinpa <hsinpa@gmail.com>
2025-09-22 13:56:31 +0800

    Done with first page of vulkan tutorial

--- SimpleTriangleApp.cpp
+++ SimpleTriangleApp.cpp
@@ -4,8 +4,10 @@
 
 #include "SimpleTriangleApp.h"
 #include <algorithm>
+#include <iostream>
 #include <print>
 #include <stdexcept>
+#include <vector>
 
 void SimpleTriangleApp::initialize(GLFWwindow *window) {
     this->window = window;
@@ -21,8 +23,45 @@ void SimpleTriangleApp::createInstance() {
     appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
     appInfo.apiVersion = VK_API_VERSION_1_1; // 1.0 also OK
 
-    VkInstanceCreateInfo ci{VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
-    ci.pApplicationInfo = &appInfo;
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    std::vector<const char*> requiredExtensions;
+    std::vector<VkExtensionProperties> extensions(glfwExtensionCount);
+
+    for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+        requiredExtensions.emplace_back(glfwExtensions[i]);
+    }
+
+    requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+    createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+    vkEnumerateInstanceExtensionProperties(nullptr, &glfwExtensionCount, extensions.data());
+
+    std::cout << "available extensions:\n";
+
+    for (const auto& extension : extensions) {
+        std::cout << '\t' << extension.extensionName << '\n';
+    }
+
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
 }
 
 void SimpleTriangleApp::mainLoop() {
